b2344b56c41e377338a98fb66ac5b3f84d53505c
EthanShipston <36234265+EthanShipston@users.noreply.github.com>
2019-03-26 13:45:30 -0400

    Add files via upload

---
+++ Contact.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _184988Summative2
+{
+    class Contact
+    {
+        //Public Variables
+        public string firstName;
+        public string lastName;
+        public int yearBorn;
+        public int monthBorn;
+        public int dayBorn;
+        public string email;
+
+        /// <summary>
+        /// Method that reads contact.txt and outputs the info in a usable format.
+        /// </summary>
+        public void ReadFromFile()
+        {
+            System.IO.StreamReader streamReader = new System.IO.StreamReader("contact.txt");
+            string temp = streamReader.ReadLine();
+            streamReader.Close();
+
+            firstName = temp.Substring(0, temp.IndexOf(","));
+            temp = temp.Remove(0, temp.IndexOf(",") + 1);
+
+            lastName = temp.Substring(0, temp.IndexOf(","));
+            temp = temp.Remove(0, temp.IndexOf(",") + 1);
+
+            int.TryParse(temp.Substring(0, temp.IndexOf(",")), out yearBorn);
+            temp = temp.Remove(0, temp.IndexOf(",") + 1);
+
+            int.TryParse(temp.Substring(0, temp.IndexOf(",")), out monthBorn);
+            temp = temp.Remove(0, temp.IndexOf(",") + 1);
+
+            int.TryParse(temp.Substring(0, temp.IndexOf(",")), out dayBorn);
+            temp = temp.Remove(0, temp.IndexOf(",") + 1);
+
+            email = temp;
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿/*Ethan Shipston
+ * DATE
+ * Program which reads a text files info and is capable of updating said info.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184988Summative2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Contact contact = new Contact();
+        public MainWindow()
+        {
+            InitializeComponent();
+            contact.ReadFromFile();
+
+            txtInOut.Text = contact.firstName
+                + "\n" + contact.lastName
+                + "\n" + contact.dayBorn.ToString()
+                + "\n" + contact.monthBorn.ToString()
+                + "\n" + contact.yearBorn.ToString()
+                + "\n" + contact.email;
+        }
+
+        private void btnSetInfo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGetAge_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime dateTime = new DateTime();
+            dateTime = DateTime.Now;
+            string temp = dateTime.Date.ToString();
+            int.TryParse(temp.Substring(0, temp.IndexOf("/")), out int tempMonth);
+            temp = temp.Remove(0, temp.IndexOf("/") + 1);
+            int.TryParse(temp.Substring(0, temp.IndexOf("/")), out int tempDay);
+            temp = temp.Remove(0, temp.IndexOf("/") + 1);
+            int.TryParse(temp.Substring(0, temp.IndexOf(" ")), out int tempYear);
+
+            int age = tempYear - contact.yearBorn;
+            if (tempMonth < contact.monthBorn)
+            {
+                age = age - 1;
+            }
+            else if (tempDay < contact.monthBorn)
+            {
+                age = age - 1;
+            }
+
+            MessageBox.Show(age.ToString() + " years old.");
+        }
+    }
+}
